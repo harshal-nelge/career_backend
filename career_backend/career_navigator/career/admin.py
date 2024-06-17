a6df268c030cab949e34d5300f18ca9709c91b4c
harshal-nelge <harshal.nelge98@gmail.com>
2024-06-17 19:48:29 +0530

    hola

--- career_backend/career_navigator/career/admin.py
+++ career_backend/career_navigator/career/admin.py
@@ -5,5 +5,5 @@ from .models import Career
 # Register your models here.
 class CareerAdmin(admin.ModelAdmin):
     list_display = ('specialization','interest','skills','certification','recommended_role')  
-    # search_fields = ['olympiad_name']  
+    
 admin.site.register(Career, CareerAdmin)
